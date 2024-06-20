a3740122993eed1bcfb91ee22a04117718930d3e
trellia003 <treccanielia03@gmail.com>
2024-06-20 09:52:32 +0200

    update merging

--- individual_code/decade/potentiometer/potentiometer.ino
+++ individual_code/decade/potentiometer/potentiometer.ino
@@ -1,48 +1,45 @@
 #define DECADE_POTENTIOMETER_PIN A0
 
-const int numReadings = 10;
-int readings[numReadings]; // Array to store the readings
-int readIndex = 0; // The index of the current reading
-int total = 0; // The running total
-int average = 0; // The average
-int decade = 0;
+int decade_potentiomiter_readings[] = { 0, 0, 0 };  // Array to store the readings
+int decade_potentiomiter_readIndex = 0;           // The index of the current reading
+int decade_potentiomiter_total = 0;               // The running total
+
+
 
 void setup() {
   Serial.begin(9600);
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0; // Initialize all the readings to 0
-  }
+  // for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  //   readings[thisReading] = 0; // Initialize all the readings to 0
+  // }
 }
 
 void loop() {
-  updatePotentioReadings();
+  // updatePotentioReadings();
   //Conditions to return decade
-  getDecade(average);
-  //Serial.println(average); // Send it to the computer
-  delay(1); // Delay in between reads for stability
+  // getDecade(average);
+  Serial.println(updatePotentioReadings());  // Send it to the computer
+  delay(1);                // Delay in between reads for stability
 }
 
-void updatePotentioReadings() {
-  total = total - readings[readIndex]; // Remove the oldest entry
-  readings[readIndex] = analogRead(DECADE_POTENTIOMETER_PIN); // Read from the sensor
-  total = total + readings[readIndex]; // Add the newest reading to the total
-  readIndex = (readIndex + 1) % numReadings; // Advance to the next position
-  average = total / numReadings; // Calculate the average
-}
+int updatePotentioReadings() {
+  int average = 0;  // The average
+  int arrayLength = sizeof(decade_potentiomiter_readings) / sizeof(decade_potentiomiter_readings[0]);
+  decade_potentiomiter_total -=  decade_potentiomiter_readings[decade_potentiomiter_readIndex];                         // Remove the oldest entry
+  decade_potentiomiter_readings[decade_potentiomiter_readIndex] = analogRead(DECADE_POTENTIOMETER_PIN);  // Read from the sensor
+  decade_potentiomiter_total += decade_potentiomiter_readings[decade_potentiomiter_readIndex];                         // Add the newest reading to the total
+  decade_potentiomiter_readIndex = (decade_potentiomiter_readIndex + 1) % arrayLength;                   // Advance to the next position
+  average = decade_potentiomiter_total / arrayLength;                               // Calculate the average
 
-int getDecade(int average) {
+  int decade = 0;
   if (120 <= average && average <= 280) {
-    decade = 3; //2010
-    Serial.println("3: 2010");
+    decade = 3;  //2010
+    // Serial.println("3: 2010");
   } else if (360 <= average && average <= 560) {
-    decade = 2; //2000
-    Serial.println("2: 2000");
+    decade = 2;  //2000
+    // Serial.println("2: 2000");
   } else if (660 <= average && average <= 820) {
-    decade = 1; //1990
-    Serial.println("1: 1990");
-  } else {
-    decade = 0;
-    Serial.println("Nothing");
+    decade = 1;  //1990
+    // Serial.println("1: 1990");
   }
   return decade;
 }
