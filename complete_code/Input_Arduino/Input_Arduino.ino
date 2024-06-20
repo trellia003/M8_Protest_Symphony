dc6a5764d1331c5414e17e03db26d796984f91b0
trellia003 <treccanielia03@gmail.com>
2024-06-20 11:00:55 +0200

    decade and billboard implemented

--- complete_code/Input_Arduino/Input_Arduino.ino
+++ complete_code/Input_Arduino/Input_Arduino.ino
@@ -4,8 +4,9 @@ the input arduino code hanles all the user inputs. if there is any change it sen
 wheras if the selection is valid and all 3 hands are placed it sends a starting message to the response arduino containing the selections
 */
 
-#include <SoftwareSerial.h>
-
+#include <SoftwareSerial.h>  //arduino communication
+#include <SPI.h>             // Include the SPI library for communication RFID
+#include <MFRC522.h>         // Include the MFRC522 library for RFID operations RFID
 
 
 /*
@@ -21,16 +22,29 @@ wheras if the selection is valid and all 3 hands are placed it sends a starting 
  * SPI MISO    MISO         12 / ICSP-1   
  * SPI SCK     SCK          13 / ICSP-3   
  */
-#define INPUT_ARDUINO_RX_PIN 10  //connect to RESPONSE_ARDUINO_TX_PIN
-#define INPUT_ARDUINO_TX_PIN 11  //connect to RESPONSE_ARDUINO_RX_PIN
-#define BUZZER_DECADE_FEEDBACK_PIN 5
-#define BUZZER_REGION_FEEDBACK_PIN 6
-#define BUZZER_PROTEST_FEEDBACK_PIN  7
-#define LED_DECADE_FEEDBACK_PIN 8
-#define LED_REGION_FEEDBACK_PIN 9
-#define LED_PROTEST_FEEDBACK_PIN 1
+#define BILLBOARD_RFID_SDA_PIN 10  // Define the RFID SDA pin
+#define BILLBOARD_RFID_RST_PIN 9   // Define the RFID Reset pin
+
+#define INPUT_ARDUINO_RX_PIN 0  //connect to RESPONSE_ARDUINO_TX_PIN
+#define INPUT_ARDUINO_TX_PIN 1  //connect to RESPONSE_ARDUINO_RX_PIN
+
+
+
+#define BUZZER_DECADE_FEEDBACK_PIN 3
+#define BUZZER_REGION_FEEDBACK_PIN 5
+#define BUZZER_PROTEST_FEEDBACK_PIN 7
+
+#define LED_DECADE_FEEDBACK_PIN 2
+#define LED_REGION_FEEDBACK_PIN 4
+#define LED_PROTEST_FEEDBACK_PIN 6
+
+
+#define DECADE_POTENTIOMETER_PIN A0
+
+
 
 SoftwareSerial serial_arduino(INPUT_ARDUINO_RX_PIN, INPUT_ARDUINO_TX_PIN);  // RX, TX pins for communication
+MFRC522 billboard_rfid(BILLBOARD_RFID_SDA_PIN, BILLBOARD_RFID_RST_PIN);     // Create MFRC522 instance
 
 
 int selected_decade[2] = { 0, 0 };   //first value is the new, second is the old
@@ -39,9 +53,18 @@ int selected_protest[2] = { 0, 0 };  //first value is the new, second is the old
 // bool is_response_running = false;    //this value is used to tell the response arduino whether to start or not
 
 
+int decade_potentiomiter_readings[] = { 0, 0, 0 };  // Array to store the readings
+int decade_potentiomiter_readIndex = 0;             // The index of the current reading
+int decade_potentiomiter_total = 0;                 // The running total
+int billboard_RFID_buffer_readings[] = { 0, 0 };    //needed to filter out the ddouble reading(0) of the RFID
+
+
 void setup() {
   Serial.begin(9600);          //start the Serial communication
   serial_arduino.begin(9600);  //start SoftwareSerial communication
+  SPI.begin();                 // Initialize SPI bus RFID
+  billboard_rfid.PCD_Init();   // Initialize MFRC522 RFID
+
 
   // Initialize buzzer pins as output
   pinMode(BUZZER_DECADE_FEEDBACK_PIN, OUTPUT);
@@ -59,4 +82,5 @@ void loop() {
   give_feedback_to_user();              //provide feedback to the user with 3 buzzer sound(when a selection is changed) and 3 led (if the selction is correct). 1led and 1 buzzer per input
   communication_to_response_arduino();  //send messages to the response arduino reset=(if a selection is changed) and start=( if the input is valud and the hands are there )
   delay(10);                            //MAYBE INCREASE IT ? FOR BUZZER SOUND AND RFID READING
+  // Serial.println("decade:"+String(selected_decade[0])+"   region:"+String(selected_region[0])+"   protest:"+String(selected_protest[0]));
 }
--- complete_code/Input_Arduino/update_selections.ino
+++ complete_code/Input_Arduino/update_selections.ino
@@ -22,13 +22,73 @@ void update_protest() {
   selected_protest[0] = new_protest;
 }
 
+int get_decade() {
+  int average = 0;  // The average
+  int arrayLength = sizeof(decade_potentiomiter_readings) / sizeof(decade_potentiomiter_readings[0]);
+  decade_potentiomiter_total -= decade_potentiomiter_readings[decade_potentiomiter_readIndex];           // Remove the oldest entry
+  decade_potentiomiter_readings[decade_potentiomiter_readIndex] = analogRead(DECADE_POTENTIOMETER_PIN);  // Read from the sensor
+  decade_potentiomiter_total += decade_potentiomiter_readings[decade_potentiomiter_readIndex];           // Add the newest reading to the total
+  decade_potentiomiter_readIndex = (decade_potentiomiter_readIndex + 1) % arrayLength;                   // Advance to the next position
+  average = decade_potentiomiter_total / arrayLength;                                                    // Calculate the average
 
-int get_decade() {  //read the decade and convert it to 0,1,2,3
+  int decade = 0;
+  if (120 <= average && average <= 280) {
+    decade = 3;  //2010
+    // Serial.println("3: 2010");
+  } else if (360 <= average && average <= 560) {
+    decade = 2;  //2000
+    // Serial.println("2: 2000");
+  } else if (660 <= average && average <= 820) {
+    decade = 1;  //1990
+    // Serial.println("1: 1990");
+  }
+  return decade;
 }
 
-int get_region() {  //read the region and convert it to 0,1,2,3
+
+int get_region() {  //read the region and convert it to 0,1europe,2asia,3notrth america
 }
 
 
-int get_protest() {  //read the protest and convert it to 0,1,2,3
+int get_protest() {  //read the protest and convert it to 0,1politics,2wages,3police brutality
+  //usually the reading of the rfid output the card number and then a 0, I tried to fix it a lot. i just used a buffer
+
+  billboard_RFID_buffer_readings[1] = billboard_RFID_buffer_readings[0];  //use the buffer to store the last two readings, if a card is read one of them should be 0 and the other not.
+  billboard_RFID_buffer_readings[0] = read_the_RFID();
+  if (billboard_RFID_buffer_readings[1] != 0) {  //if the last element is not 0, we want to use that card number
+    return billboard_RFID_buffer_readings[1];
+  } else {  //otherwise if the buffer[1] is 0, the buffer[0] is either a 0 oe a number, and we just use that because yes.
+    return billboard_RFID_buffer_readings[0];
+  }
+}
+
+
+int read_the_RFID() {
+  int billboard_protest_code = 0;  // Initialize the protest code to 0
+
+  // Check if a new RFID card is present and read its serial number
+  if (billboard_rfid.PICC_IsNewCardPresent() && billboard_rfid.PICC_ReadCardSerial()) {
+    String content = "";  // Initialize an empty string to store the UID
+
+    // Loop through each byte of the UID and convert it to a hexadecimal string
+    for (byte i = 0; i < billboard_rfid.uid.size; i++) {
+      // Append a leading zero if the byte is less than 0x10 for proper formatting
+      content.concat(String(billboard_rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+      // Append the byte in hexadecimal format
+      content.concat(String(billboard_rfid.uid.uidByte[i], HEX));
+    }
+    content.toUpperCase();  // Convert UID string to uppercase for uniformity
+
+    // Check if the UID matches a known protest code
+    if (content.substring(1) == "73 F3 0B 1A") {         // First UID
+      billboard_protest_code = 1;                        // Set protest code to 1
+    } else if (content.substring(1) == "03 B7 86 15") {  // Second UID
+      billboard_protest_code = 2;                        // Set protest code to 2
+    } else if (content.substring(1) == "D3 6F 65 95") {  // Third UID
+      billboard_protest_code = 3;                        // Set protest code to 3
+    } else {
+      billboard_protest_code = 0;  // Unknown UID, set protest code to 0
+    }
+  }
+  return billboard_protest_code;  // Return the protest code
 }
