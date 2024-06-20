fecfe6a92d232a74d03df86afd7e96b6910e9627
ngxuanbminh <163402925+ngxuanbminh@users.noreply.github.com>
2024-06-20 10:57:45 +0200

    Update flag_control.ino

--- individual_code/Flag/flag_control/flag_control.ino
+++ individual_code/Flag/flag_control/flag_control.ino
@@ -7,20 +7,20 @@ Servo flagServo;  // Create a Servo object
 
 void setup() {
   flagServo.attach(FLAG_SERVO_PIN);  // Attach the ATS90M 360 servo to pin 9
-  pinMode(FLAG_FAN_PIN, OUTPUT);  // Set the fan control pin as output
+  //pinMode(FLAG_FAN_PIN, OUTPUT);  // Set the fan control pin as output
   digitalWrite(FLAG_FAN_PIN, LOW);  // Ensure the fan is off initially
 }
 
 void loop() {
   // Stop the servo
   flagServo.write(90);  // 90 degrees (1500 microseconds) stops the servo
-  digitalWrite(FLAG_FAN_PIN, LOW);  // Turn the fan off
+  //digitalWrite(FLAG_FAN_PIN, LOW);  // Turn the fan off
   delay(1000);  // Wait for 1 second
 
   // Rotate servo in one direction - RAISE FLAG
   flagServo.write(180);  // Less than 90 degrees for one direction
-  digitalWrite(FLAG_FAN_PIN, HIGH);  // Ensure the fan is on
-  delay(800);  // Servo spin time 
+  //digitalWrite(FLAG_FAN_PIN, HIGH);  // Ensure the fan is on
+  delay(700);  // Servo spin time 
 
   // Stop the servo - KEEP THE FLAG VISIBLE
   flagServo.write(90);  // Stop the servo
@@ -28,9 +28,8 @@ void loop() {
 
   // Rotate servo in the opposite direction - LOWER FLAG
   flagServo.write(0);  // More than 90 degrees for the opposite direction
-  digitalWrite(FLAG_FAN_PIN, LOW);  // Turn the fan off
-  
-  delay(800);  // Servo spin time
+  //digitalWrite(FLAG_FAN_PIN, LOW);  // Turn the fan off
+  delay(750);  // Servo spin time
 
   // Stop the servo
   flagServo.write(90);  // Stop the servo
